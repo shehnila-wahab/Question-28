c8139b8f6076fea3e1e2fdcdd895cff7cf899700
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-05-25 23:17:11 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,13 @@
+let age:number=25
+if(age<2){
+    console.log("the person is a baby.");
+}else if(age>=2 && age<4){
+    console.log("the person is a toddler.")
+}else if(age>=4 && age <13){
+    console.log("print a message that the person is a kid.")
+}else if(age>=13 &&age <20){
+    console.log("the person is a teenager.")
+}else if(age>=20 && age<65){
+    console.log("the person is an adult..")
+}else
+console.log("the person is an elder.")
